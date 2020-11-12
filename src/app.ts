35e4a520f5b2d51f76a21e3a65cfe8499645376e
zperee <elia.perenzin@bluewin.ch>
2020-11-12 19:20:32 +0100

    added cron to api

--- src/app.ts
+++ src/app.ts
@@ -8,25 +8,21 @@ import connectDB from "./config/database";
 dotenv.config({
   path: '.env'
 });
-
-// Express server application class.
-class Server {
-  public app = express();
-  public router = MasterRouter;
-}
+var cors = require('cors');
+let app = express();
+app.use(cors());
+let router = MasterRouter;
 
 // Connect to MongoDB
 connectDB();
 
-// initialize server app
-const server = new Server();
 
 // make server app handle any route starting with '/api'
-server.app.get('/', (req: Request, res: Response, next: NextFunction) => {res.status(200).json("Welcome to the CarPar API.");});
-server.app.use('/api', server.router);
+app.get('/', (req: Request, res: Response, next: NextFunction) => {res.status(200).json("Welcome to the CarPar API.");});
+app.use('/api', router);
 
 // make server app handle any error
-server.app.use((err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
+app.use((err: ErrorHandler, req: Request, res: Response, next: NextFunction) => {
   res.status(err.statusCode || 500).json({
     status: 'error',
     statusCode: err.statusCode,
@@ -36,6 +32,6 @@ server.app.use((err: ErrorHandler, req: Request, res: Response, next: NextFuncti
 
 // make server listen on some port
 ((port = process.env.PORT || 5000) => {
-  server.app.listen(port, () => console.log(`> Listening on port ${port}`));
+  app.listen(port, () => console.log(`> Listening on port ${port}`));
 })();
 
